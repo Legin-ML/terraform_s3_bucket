903393cf98c736932021e920d6b04284dd975784
Legin <leginml2004@gmail.com>
2024-01-28 19:40:05 +0530

    fix: Shortened and clarified output variables for user

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "s3_bucket_id" {
+output "id" {
     description = "ID of the Bucket."
     value = aws_s3_bucket.s3_bucket_mod.id
 }
 
-output "s3_bucket_arn" {
+output "arn" {
     description = "ARN of the Bucket (Format: arn:aws:s3:::bucketname)."
     value = aws_s3_bucket.s3_bucket_mod.arn
 }
 
-output "s3_bucket_domain_name" {
+output "domain_name" {
     description = "Domain name of the Bucket"
     value = aws_s3_bucket.s3_bucket_mod.bucket_domain_name
 }
